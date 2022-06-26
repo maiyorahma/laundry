3ae868f1426c9876ddbe0adeeaaa70df6471ef39
ASUS <12050122022@students.uin-suska.ac.id>
2022-06-26 11:57:48 +0700

    commit pertama

---
+++ application/views/admin/home.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="center">
+        
+        <center>
+            <img src="<?= base_url();?>assets/dist/img/icon66.png" alt="icon 66" style="opacity: .9"> 
+        </center>
+        
+        <center>
+            <span style="font-size:45px; margin-bottom:-20px"><b><font color="#6495ed">SISTEM INFORMASI LAUNDRY</b></span><br>
+            <span style="font-size:40px; margin-top:-20px"><b><?= strtoupper($data->nama_laundry);?></b></span><br>
+            <span style="font-size:17px; font-style:italic;">Alamat: <?= $data->alamat_laundry?> Tlp: <?= $data->no_tlp?></span>
+        </center>
+    </div>
+</div>
